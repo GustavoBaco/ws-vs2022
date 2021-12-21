7a882d56f5b3495ab86ffc310bcc07a19998502c
Gustavo Baco Manciope <89161781+GustavoBaco@users.noreply.github.com>
2021-12-21 15:06:19 -0300

    Exercicio
    Exercício para treinar o uso das funções matemáticas apresentadas no curso.

---
+++ Aula16_ProcessamentoDeDados/Aula23_ExercicioProposto_Parte1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula24_ExercicioResolvido2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double b, a, perimetro, diagonal,area;
+            b = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            a = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            area = b * a;
+            perimetro = ((2 * b) + (2 * a));
+            diagonal = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            Console.WriteLine("Área = " + area.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Perimetro = " + perimetro.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("Diagonal = "+diagonal.ToString("F4",CultureInfo.InvariantCulture));
+        }
+    }
+}
