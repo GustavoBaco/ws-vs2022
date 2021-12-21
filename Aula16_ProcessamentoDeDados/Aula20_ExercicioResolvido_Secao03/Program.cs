b6b9d60072f6792f71ef435018978c2b82db7dbf
Gustavo Baco Manciope <89161781+GustavoBaco@users.noreply.github.com>
2021-12-21 14:27:50 -0300

    Aula20

---
+++ Aula16_ProcessamentoDeDados/Aula20_ExercicioResolvido_Secao03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula20_ExercicioResolvido_Secao03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a lagura do terreno: ");
+            double L = double.Parse(Console.ReadLine());
+            Console.WriteLine("Informe o comprimento do terreno: ");
+            double C = double.Parse(Console.ReadLine());
+            double area = L * C;
+            Console.WriteLine("Informe o valor do metro quadrado de um terreno: ");
+            double VlrArea = double.Parse(Console.ReadLine());
+            double VlrTerreno = area * VlrArea;
+            Console.WriteLine("Área = " + area.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Valor do terreno = " + VlrTerreno.ToString("F2",CultureInfo.InvariantCulture));
+        }
+    }
+}
