d3331d3c4b8e89c8d65e9405f4128a188bd2dfd5
Gustavo Baco Manciope <89161781+GustavoBaco@users.noreply.github.com>
2021-12-22 17:12:39 -0300

    Aula_35

---
+++ Aula16_ProcessamentoDeDados/Aula35_Exercicio_Resolvido03/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Aula35_Exercicio_Resolvido03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+            string[] vet;
+            vet = Console.ReadLine().Split(' ');
+
+            
+            a = int.Parse(vet[0]);
+            
+            b = int.Parse(vet[1]);
+            
+            c = int.Parse(vet[2]);
+
+            if(a < b && a < c)
+            {
+                Console.WriteLine("MENOR = " + a);
+            }
+            else if (b < a && b < c)
+            {
+                Console.WriteLine("MENOR = " + b);
+            }
+            else if (c < a && c < b)
+            {
+                Console.WriteLine("MENOR = " + c);
+            }
+            else
+            {
+                Console.WriteLine("MENOR = " + a);
+            }
+
+        }
+    }
+}
