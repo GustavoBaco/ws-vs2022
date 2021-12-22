007f370b35fcbe7840a2afd440bb111a3eb1337f
Gustavo Baco Manciope <89161781+GustavoBaco@users.noreply.github.com>
2021-12-22 16:23:27 -0300

    Aulas

---
+++ Aula16_ProcessamentoDeDados/Aula33_Exercicio_Resolvido01_EstruturaCondicional/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula33_Exercicio_Resolvido01_EstruturaCondicional
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double N1, N2, sum;
+
+            Console.WriteLine("Informe a primeira nota do aluno: ");
+            N1 = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Informe a segunda nota do aluno: ");
+            N2 = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            sum = N1 + N2;
+
+            if (sum < 60.00)
+            {
+                Console.WriteLine("Nota final = " + sum.ToString("F2",CultureInfo.InvariantCulture));
+                Console.WriteLine("Aluno Reprovado!");
+            }
+            else
+            {
+                Console.WriteLine("Nota final = " + sum.ToString("F2", CultureInfo.InvariantCulture));
+                Console.WriteLine("Aluno Aprovado!");
+            }
+
+        }
+    }
+}
---
+++ Exercicio_1009/Exercicio_1009/Exercicio_1009/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Exercicio_1009
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome;
+            double salarioFixo, totalReceber, vendasEfetuadas;
+
+            nome = Console.ReadLine();
+            salarioFixo = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            vendasEfetuadas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            totalReceber = vendasEfetuadas * 15 / 100 + salarioFixo;
+
+            Console.WriteLine("TOTAL = R$ " + totalReceber.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
