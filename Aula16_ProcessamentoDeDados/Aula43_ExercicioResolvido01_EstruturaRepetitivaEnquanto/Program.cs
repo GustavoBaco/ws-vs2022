1abc9d891e2dcae2239dc576529f906cb91c3cf3
Gustavo Baco Manciope <89161781+GustavoBaco@users.noreply.github.com>
2021-12-23 16:41:56 -0300

    23/12

---
+++ Aula16_ProcessamentoDeDados/Aula43_ExercicioResolvido01_EstruturaRepetitivaEnquanto/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Aula43_ExercicioResolvido01_EstruturaRepetitivaEnquanto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            string[] vet;
+
+            vet = Console.ReadLine().Split(' ');    
+            x = int.Parse(vet[0]);
+            y = int.Parse(vet[1]);
+
+            while(x != y)
+            {
+                if (x < y)
+                {
+                    Console.WriteLine("Crescente");
+                }
+                else
+                {
+                    Console.WriteLine("Decrescente");
+                }
+                vet = Console.ReadLine().Split(' ');
+                x = int.Parse(vet[0]);
+                y = int.Parse(vet[1]);
+            }
+        }
+    }
+}
