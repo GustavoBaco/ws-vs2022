54efd8822912d51d96d3e982d625645e43f10ab6
Gustavo Baco Manciope <89161781+GustavoBaco@users.noreply.github.com>
2021-12-23 12:15:58 -0300

    23/12

---
+++ Aula16_ProcessamentoDeDados/Aula40_EstruturaRepetitivaEnquanto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aula40_EstruturaRepetitivaEnquanto_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x,soma;
+
+            x = int.Parse(Console.ReadLine());
+            soma = 0;
+            while (x != 0) // Enquanto a condição for verdadeira, entra no laço de repetição. Quando for falso,pula fora do laço.
+            {
+                soma = soma + x;
+                x = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine(soma); ;
+            Console.ReadLine();
+        }
+    }
+}
