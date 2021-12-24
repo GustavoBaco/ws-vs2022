4422026e8858ebb5b7c27817e3b24f2a9b823146
Gustavo Baco Manciope <89161781+GustavoBaco@users.noreply.github.com>
2021-12-24 11:53:31 -0300

    Aulas 24-12

---
+++ Aula16_ProcessamentoDeDados/Aula46_EstruturaRepetitivaPara/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Aula46_EstruturaRepetitivaPara
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N, x, soma;
+
+            N = int.Parse(Console.ReadLine());
+            soma = 0;
+            for (int i=1 ; i <=N; i++ ) // i++ = i= i+i
+            {
+                x = int.Parse(Console.ReadLine());
+                soma = soma + x;
+            }
+            Console.WriteLine(soma);
+            Console.ReadLine();
+        }
+    }
+}
