3134e1e4ff647ffdf3e3296b16248c1081699a6e
Gustavo Baco Manciope <89161781+GustavoBaco@users.noreply.github.com>
2021-12-27 15:39:37 -0300

    Aula 58

---
+++ Aula16_ProcessamentoDeDados/Aula58_ExercicioResolvido02_Vetores/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Globalization;
+
+namespace Aula58_ExercicioResolvido02_Vetores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int N;
+            double[] vet;
+
+            N = int.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+            vet = new double[N];
+
+            string[] s = Console.ReadLine().Split(' ');
+            for (int i = 0; i < N; i++)
+            {
+                vet[i] = double.Parse(s[i],CultureInfo.InvariantCulture);
+            }
+
+            for (int i = 0;i < N; i++)
+            {
+                Console.Write(vet[i] + " ");
+            }
+            Console.WriteLine();
+
+            double soma = 0.0;
+            for (int i = 0; i<N; i++)
+            {
+                soma = soma + vet[i];
+            }
+            double media = soma / N;
+            Console.WriteLine(soma.ToString("F2",CultureInfo.InvariantCulture));
+            Console.WriteLine(media.ToString("F2",CultureInfo.InvariantCulture));
+            
+          
+        }
+    }
+}
