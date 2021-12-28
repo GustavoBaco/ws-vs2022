2adc9f5dcf93d0e44b06c61e300cddcafe769cd2
Gustavo Baco Manciope <89161781+GustavoBaco@users.noreply.github.com>
2021-12-28 09:45:33 -0300

    Aula

---
+++ Aula16_ProcessamentoDeDados/Aula60_Foreach/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Aula60_Foreach
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            n = int.Parse(Console.ReadLine());
+            string[] vet = new string[n];
+
+            for (int i = 0; i<n; i++)
+            {
+                vet[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Nomes lidos: ");
+            foreach (string elemento in vet)
+            {
+                Console.WriteLine(elemento);
+            }
+        }
+        
+        
+    }
+}
