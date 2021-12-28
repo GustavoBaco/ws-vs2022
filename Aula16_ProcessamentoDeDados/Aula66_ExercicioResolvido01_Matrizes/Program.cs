f3c7bf8a1322a81c610e28bbbc89d3a022ffab66
Gustavo Baco Manciope <89161781+GustavoBaco@users.noreply.github.com>
2021-12-28 15:29:17 -0300

    ExercicioResolvido_Matriz

---
+++ Aula16_ProcessamentoDeDados/Aula66_ExercicioResolvido01_Matrizes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Aula66_ExercicioResolvido01_Matrizes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] A;
+            int N = int.Parse(Console.ReadLine());
+            A = new int[N, N];
+
+            for(int i = 0; i < N; i++)
+            {
+                string [] s = Console.ReadLine().Split(' ');
+                for (int j = 0; j<N; j++)
+                {
+                    A[i,j] = int.Parse(s[j]);
+                }
+            }
+            Console.WriteLine("Diagonal Pricipal: ");
+            for(int i = 0; i < N; i++)
+            {
+                Console.Write(A[i, i] + " ");
+            }
+            Console.WriteLine();
+
+            int cont = 0; 
+            for(int i = 0; i <N; i++)
+            {
+                for (int j = 0;j<N; j++)
+                {
+                    if (A[i,j] < 0)
+                    {
+                        cont++;
+                    }
+                }
+            }
+            Console.WriteLine("Quantidade de negativos = " + cont);
+        }
+    }
+}
